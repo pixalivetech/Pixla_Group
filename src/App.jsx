726af1d6a224148544a18b7fe8ad906feafc8362
shibu-pix <shibu.pixalive@gmail.com>
2025-10-13 16:08:59 +0530

    REname and section connection

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import Header from './Components/Header/header';
 import Footer from "./Components/Footer/footer";
-import Hero from "./Components/Home/hero";
-import About from "./Components/Home/about";
-import Division from "./Components/Home/division";
-import Products from "./Components/Home/Products";
-import Keys from "./Components/Home/Key";
-import Partners from "./Components/Home/Partner";
-import Acadamy from "./Components/Home/Acadamy";
-import Review from "./Components/Home/Review";
-import Blogs from "./Components/Home/Blog";
-import Path from "./Components/Home/pathway";
+import Home from "./Pages/Home";
 
 
 
@@ -19,16 +10,9 @@ function App() {
   return (
     <Router>
       <Header />
-      <Hero/>
-      <About/>
-      <Division/>
-      <Products/>
-      <Keys/>
-      <Partners/>
-      <Acadamy/>
-      <Review/>
-      <Blogs />
-      <Path/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes>
       <Footer/>
     </Router>
   );
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Hero from "./../Components/Home/Hero";
+import About from "./../Components/Home/About";
+import Division from "./../Components/Home/Division";
+import Products from "./../Components/Home/Products";
+import Keys from "./../Components/Home/Key";
+import Partners from "./../Components/Home/Partner";
+import Acadamy from "./../Components/Home/Acadamy";
+import Review from "./../Components/Home/Review";
+import Blogs from "./../Components/Home/Blog";
+import Path from "./../Components/Home/pathway"; 
+
+const Home = () => {
+  return (
+    <div>
+     <Hero/>
+      <About/>
+      <Division/>
+      <Products/>
+      <Keys/>
+      <Partners/>
+      <Acadamy/>
+      <Review/>
+      <Blogs />
+      <Path/> 
+    </div>
+  )
+}
+
+export default Home
