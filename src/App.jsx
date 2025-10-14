c11562601402fdd05fcab8653db5d294e0c13251
Mayakannan C <146314212+mayakannan2002@users.noreply.github.com>
2025-10-14 12:34:52 +0530

    mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,31 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Components/Header/header";
-import Footer from "./Components/Footer/footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Contact from './Components/Contact/Contact'
+import FmcgPage from './Pages/FmcgPage';
 
-// Pages
-import Hero from "./Components/Home/hero";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+
 import AboutUs from "./Pages/AboutUs";
 import Retail from "./Pages/Retail";
-import About from "./Components/Home/about";
-import Division from "./Components/Home/division";
+import Itservice from "./Pages/Itservice"
+import Home from "./Pages/Home";
 import Pixla from "./Pages/Pixla";
 import Pixalive from "./Pages/Pixalive";
 import Pixlakart from "./Pages/Pixlakart";
 import PixlaAcademy from "./Pages/Pixlaacademy";
 import Pixclick from "./Pages/Pixclick";
-import Contact from './Components/Contact/Contact'
-import FmcgPage from './Pages/FmcgPage';
-import Itservice from "./Pages/Itservice"
-import Home from "./Pages/Home";
 
 
 
 function App() {
   return (
     <Router>
-      {/* Common Header */}
       <Header />
-
-      {/* Page Routes */}
       <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <Hero />
-              <About />
-              <Division />
-            </>
-          }
-        />
+
+        <Route path="/" element={<Home />} />
+
         <Route path="/about" element={<AboutUs />} />
         <Route path="/retail" element={<Retail />} />
         <Route path="/pixla" element={<Pixla />} />
@@ -46,21 +33,12 @@ function App() {
         <Route path="/pixlakart" element={<Pixlakart />} />
         <Route path="/pixlaacademy" element={<PixlaAcademy />} />
         <Route path="/pixclick" element={<Pixclick />} />
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/retail" element={<Retail />} />
-
 
 
         <Route path="/contact" element={<Contact />} />
         <Route path="/fmcg" element={<FmcgPage />} />
         <Route path="/it-services" element={<Itservice />} />
       </Routes>
-
-      {/* Common Footer */}
       <Footer />
 
 
